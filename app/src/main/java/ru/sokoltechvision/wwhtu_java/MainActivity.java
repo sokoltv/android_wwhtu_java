1f95f78413f7ec1b5f3fecd234e8c2d4f16d8467
Arthur <sokoltvgames@mail.ru>
2021-10-12 07:45:31 +0300

    Debug update

--- app/src/main/java/ru/sokoltechvision/wwhtu_java/MainActivity.java
+++ app/src/main/java/ru/sokoltechvision/wwhtu_java/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import org.joda.time.LocalTime;
 
+import java.security.PublicKey;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,7 +28,26 @@ public class MainActivity extends AppCompatActivity {
     private String[] EVE1;
     private String[] NIGHT1;
     private String[] OVER1;
+  /*  public int _NT;
+    public int _MTB;
+    public int _MTE;
+    public int _DTB;
+    public int _DTE;
+    public int _DayTB;
+    public int _DayTE;
+    public int _EvTB;
+    public int _EvTE;
+    public int _NightTB;
+    public int _nightTE;
+    public int _OverTB;
+    public int _OverTE;
 
+*/
+   public LocalTime NowTime = LocalTime.now();
+   public int _NTh = NowTime.getHourOfDay();
+   public int _NTm = NowTime.getMinuteOfHour();
+   public int _NTs = NowTime.getSecondOfMinute();
+   public int _NTms = NowTime.getMillisOfSecond();
   /*  public String[] Tmorn;
 
     //утро ms
@@ -65,11 +85,56 @@ public class MainActivity extends AppCompatActivity {
 
 
         //выдираем время
-        DateFormat DF = new SimpleDateFormat("HH:mm");
-        String date = DF.format(Calendar.getInstance().getTime());
+   //     DateFormat DF = new SimpleDateFormat("HH:mm");
+    //    String date = DF.format(Calendar.getInstance().getTime());
+
+
+        // Утро
+        LocalTime MornTimeBegin = new LocalTime(8,0,0,0);
+
+        int _MornT_Bh = MornTimeBegin.getHourOfDay();
+
+        LocalTime MornTimeEnd = new LocalTime(12,0,59,99);
+
+        int _MornT_Eh = MornTimeEnd.getHourOfDay();
+        int _MornT_Em = MornTimeEnd.getMinuteOfHour();
+        int _MornT_Es = MornTimeEnd.getSecondOfMinute();
+        int _MornT_Ems = MornTimeEnd.getMillisOfSecond();
+
+        // Обед
+        LocalTime DinTimeBegin = new LocalTime(12,1,0,0);
+        LocalTime DinTimeEnd = new LocalTime(15,0,59,99);
+
+        // День
+        LocalTime DayTimeBegin = new LocalTime(15,1,0,0);
+        LocalTime DayTimeEnd = new LocalTime(17,30,59,99);
+
+        // Вечер
+        LocalTime EveTimeBegin = new LocalTime(17,31,0,0);
+        LocalTime EveTimeEnd = new LocalTime(20,30,59,99);
+
+        // Ночь
+        LocalTime NightTimeBegin = new LocalTime(20,31,0,0);
+        LocalTime NightTimeEnd = new LocalTime(23,59,59,99);
+
+        // OverNight
+        LocalTime OverTimeBegin = new LocalTime(0,0,0,0);
+        LocalTime OverTimeEnd = new LocalTime(7,59,59,99);
+
+        int _OverTB_h = OverTimeBegin.getHourOfDay();
+        int _OverTB_m = OverTimeBegin.getMinuteOfHour();
+        int _OverTB_s = OverTimeBegin.getSecondOfMinute();
+        int _OverTB_ms = OverTimeBegin.getMillisOfSecond();
+
+        int _OverTE_h = OverTimeEnd.getHourOfDay();
+        int _OverTE_m = OverTimeEnd.getMinuteOfHour();
+        int _OverTE_s = OverTimeEnd.getSecondOfMinute();
+        int _OverTE_ms = OverTimeEnd.getMillisOfSecond();
 
 
 
+
+        // Опять костыли
 
 
 
@@ -83,30 +148,58 @@ public class MainActivity extends AppCompatActivity {
             {
                 YOBA();
 
-              //  date.compareTo()
+                if (_NTh >= _OverTB_h){
+                    if (_NTh <= _OverTE_h){
+                        _OVER();
+                    }
+                    if (_NTh <= _MornT_Eh && _NTh >= _MornT_Bh){
+                        _MORN();
+                    }
+
+                }
+                else {
+
+                    _DAY1();
+                }
+
+     /*           if (_NT2 > _OverTB2){
+                    if (_NT2 < _OverTE2){
+
+                        _OVER();
+
+                    }
+                    else if (_NT <= _MTE){
+
+                        _MORN();
+                    }
+                    else if (_NT <= _DTE){
+
+                        _DIN();
+                    }
+                    else if (_NT <= _DayTE){
+                        _DAY1();
+                    }
+                    else if (_NT <= _EvTE){
+
+                        _EVE();
+                    }
+                    else if (_NT <= _nightTE){
+
+                        _NIGHT();
+                    }
+                }
+                else {
+
+                    _DAY1();
+                }
+*/
+                //  date.compareTo()
                 //сраный алгоритм времени
-                 _DAY1();
+                 // _DAY1();
             }
         });
     }
 
-    //ебанная хуйня не работает, удалить потом
-  /*  public void getAllResourceId() {
-        final R.id idResources = new R.id();
-        final Class<R.id> c = R.id.class;
-        final java.lang.reflect.Field[] fields = c.getDeclaredFields();
-        for (Field field : fields) {
-            final int resourceId;
-            try {
-                resourceId = field.getInt(idResources);
-                Log.i("ID", "resourceId: " + resourceId + " Name:" + field.getName());
-
-            } catch (Exception e) {
-                Log.e("Exception", e.getLocalizedMessage());
-            }
-        }
-    }
-*/
 
 
 
@@ -172,14 +265,25 @@ public class MainActivity extends AppCompatActivity {
         LocalTime now = LocalTime.now();
         LocalTime start = new LocalTime(13,0,0,0);
         LocalTime end = new LocalTime(22,0,0,0);
+        int _n = 0;
+        int _s = 0;
+        int _e = 0;
+
+        long nowT = now.getMillisOfSecond();
+        start.getValue(_s);
+        end.getValue(_e);
+
+        LocalTime OverTimeBeginLog = new LocalTime(0,0,0,0);
+        LocalTime OverTimeEndLog = new LocalTime(7,59,59,99);
+
 
         TextView outnow = (TextView) findViewById(R.id.textView_tmNow);
         TextView outstart = (TextView) findViewById(R.id.textView_tmstart);
         TextView outend = (TextView) findViewById(R.id.textView_tmEnd);
 
         String nowS = now.toString();
-        String strS = start.toString();
-        String endS= end.toString();
+        String strS = OverTimeBeginLog.toString();
+        String endS= OverTimeEndLog.toString();
 
         outnow.setText(nowS);
         outstart.setText(strS);
